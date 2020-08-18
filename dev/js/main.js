35c9f858a9c06c545e1ce8d4144aa230f7378703
grammlaman <K1j1u1j1n1b1g1>
2020-08-19 03:08:37 +0600

    Сверстал половину баннеров, форму подписки, поправил футер хедер

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,3 +3,19 @@ document.querySelector('.head-burger').addEventListener('click',function (e) {
     let cont = document.querySelector('header');
     cont.classList.toggle('head-nav-show')
 });
+
+function headScrolled() {
+    if(window.pageYOffset){
+        if(!document.querySelector('.head').classList.contains('head-scrolled')){
+            document.querySelector('.head').classList.add('head-scrolled')
+        }
+    } else {
+        if(document.querySelector('.head').classList.contains('head-scrolled')){
+            document.querySelector('.head').classList.remove('head-scrolled')
+        }
+    }
+}
+headScrolled();
+window.addEventListener('scroll',function (e) {
+    headScrolled();
+});
